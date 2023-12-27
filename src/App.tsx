0a183e9912644120681e66d1be50b4eeb0fea232
Aya <90342033+Aya-X@users.noreply.github.com>
2023-12-27 15:37:27 +0800

    layout h100

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
@@ -7,7 +8,12 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 const GridItem = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(1),
+  padding: theme.spacing(4),
+  height: '100%',
+
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
   textAlign: 'center',
 
   ...theme.typography.body2,
@@ -18,8 +24,8 @@ const GridItem = styled(Paper)(({ theme }) => ({
 function FormRow() {
   return (
     <>
-      <Grid item xs={4}>
-        <GridItem>Item1</GridItem>
+      <Grid item xs={2} sm={3} md={4} sx={{ height: '20%' }}>
+        <GridItem>Item Item</GridItem>
       </Grid>
     </>
   );
@@ -27,25 +33,25 @@ function FormRow() {
 
 function NestedGrid() {
   return (
-    <Box sx={{ flexGrow: 1, p: 8 }}>
-      <Grid container spacing={0}>
-        <Grid container item spacing={1} sx={{ backgroundColor: 'red' }}>
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-          <FormRow />
-        </Grid>
+    <Box
+      sx={{
+        flexGrow: 1,
+        p: 8,
+        height: '80%',
+        alignItems: 'stretch',
+      }}
+    >
+      <Grid
+        container
+        spacing={0}
+        // direction="column"
+        columns={{ xs: 3, sm: 9, md: 12 }}
+        justifyContent={'center'}
+        sx={{ height: '100%' }}
+      >
+        {Array.from(Array(15)).map((_, index) => (
+          <FormRow key={index} />
+        ))}
       </Grid>
     </Box>
   );
