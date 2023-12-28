b39ba22442e78380e8203d3672dfcf2482220289
Aya <90342033+Aya-X@users.noreply.github.com>
2023-12-28 17:16:45 +0800

    Update GridSortable.tsx

--- src/GridSortable.tsx
+++ src/GridSortable.tsx
@@ -3,14 +3,18 @@ import { ReactSortable } from 'react-sortablejs';
 
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
+import FilledInput from '@mui/material/FilledInput';
 import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
+import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import ClearIcon from '@mui/icons-material/Clear';
+import SaveIcon from '@mui/icons-material/Save';
 
 const hoverEffectStyles = {
   transition: 'transform .3s linear',
@@ -28,13 +32,67 @@ const GridAddItem = () => {
 };
 // edf of GridAddItem
 
-const GridItem = ({ item, index, handleDelete }) => {
+const RenderStockName = (props) => {
+  const { stockName, handleSave } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [editValue, setEditValue] = useState(stockName);
+
+  const handleEditStockName = () => {
+    console.log('handleEditStockName:', editValue);
+    handleSave(editValue);
+    setIsEditing(false);
+  };
+
+  return (
+    <>
+      {isEditing ? (
+        <FilledInput
+          id="stockName"
+          aria-describedby="stockName"
+          size="small"
+          // defaultValue={stockName}
+          value={editValue}
+          onChange={(e) => setEditValue(e.target.value)}
+          onMouseLeave={() => setIsEditing(false)}
+          onKeyDownCapture={(e) => {
+            if (e.key === 'Enter') {
+              handleEditStockName();
+            }
+          }}
+          sx={{ p: 0, textAlign: 'center', '& .MuiInputBase-input': { p: 1 } }}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="save stock name"
+                onClick={handleEditStockName}
+              >
+                <SaveIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      ) : (
+        <Button
+          variant="text"
+          color="inherit"
+          size="large"
+          onClick={() => setIsEditing(true)}
+        >
+          {stockName}
+        </Button>
+      )}
+    </>
+  );
+};
+
+const GridItem = ({ item, index, handleDelete, handleSaveStockName }) => {
   const theme = useTheme();
 
   const cardStyles = {
     display: 'flex',
     flexDirection: 'column',
     height: '100%',
+    // aspectRatio: '1 / 1',
     border: '1px solid #ccc',
     borderRadius: 0,
     bgcolor: item?.backgroundColor,
@@ -86,7 +144,13 @@ const GridItem = ({ item, index, handleDelete }) => {
           </IconButton>
         </CardActions>
 
-        <CardContent sx={{ flexGrow: 1, py: 4, textAlign: 'center' }}>
+        <CardContent
+          sx={{
+            flexGrow: 1,
+            // pt: '20%',
+            textAlign: 'center',
+          }}
+        >
           <Typography
             variant="h5"
             component="div"
@@ -95,7 +159,17 @@ const GridItem = ({ item, index, handleDelete }) => {
               color: !item?.stockId ? theme.palette.text.disabled : 'inherit',
             }}
           >
-            {item?.stockId ? item?.stockName : item?.stockSeq + 1}
+            {item?.stockId ? (
+              <RenderStockName
+                stockName={item?.stockName}
+                handleSave={(newName) => {
+                  console.log('RenderStockName handleSave:', index, newName);
+                  handleSaveStockName(index, newName);
+                }}
+              />
+            ) : (
+              item?.stockSeq + 1
+            )}
           </Typography>
           <Typography sx={{ fontSize: 14 }}>
             {item?.stockId
@@ -147,6 +221,12 @@ const GridSortable = () => {
     setList([{ ...list[0], stockList: updatedList }]);
   };
 
+  const handleSaveStockName = (index, newName) => {
+    const updatedList = [...list[0].stockList];
+    updatedList[index] = { ...updatedList[index], stockName: newName };
+    setList([{ ...list[0], stockList: updatedList }]);
+  };
+
   return (
     <Box
       sx={{
@@ -154,8 +234,7 @@ const GridSortable = () => {
         p: 8,
 
         border: '1px solid #ccc',
-        borderRadius: '12px',
-        bgcolor: '#cfe8fc',
+        bgcolor: '#ccc',
         shadow:
           '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
 
@@ -205,6 +284,10 @@ const GridSortable = () => {
               item={item}
               index={index}
               handleDelete={handleDelete}
+              handleSaveStockName={(index, newName) => {
+                console.log('GridItem handleSaveStockName:', index, newName);
+                handleSaveStockName(index, newName);
+              }}
             />
           ))}
         </ReactSortable>
