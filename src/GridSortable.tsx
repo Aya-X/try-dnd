69947086e9db7146b617a902b330037c05451919
Aya <90342033+Aya-X@users.noreply.github.com>
2023-12-28 14:47:32 +0800

    update stockSeq

--- src/GridSortable.tsx
+++ src/GridSortable.tsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';
 import { ReactSortable } from 'react-sortablejs';
 
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-
-import './assets/styles/grid.css';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import DragHandleIcon from '@mui/icons-material/DragHandle';
 
 const GridAddItem = () => {
   return (
@@ -21,43 +21,115 @@ const GridAddItem = () => {
   );
 };
 
-const GridItem = ({ user }) => {
-  return (
-    <div className="grid-items card shadow-sm rounded-3 overflow-hidden">
-      <div className="card-body">
-        <h3 className="h5 card-title m-0">{user.name}</h3>
-      </div>
+const GridItem = ({ item }) => {
+  const theme = useTheme();
 
-      <div className="card-footer d-flex justify-content-between">
-        <span>{user.id}</span>
-        <span className="btn btn-white border shadow-sm dragHandle">=</span>
-      </div>
-    </div>
+  const cardStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    border: '1px solid #ccc',
+    borderRadius: 0,
+    bgcolor: item?.backgroundColor,
+    color: theme.palette.getContrastText(item?.backgroundColor),
+  };
+
+  return (
+    <>
+      <Card sx={cardStyles}>
+        <CardActions sx={{ p: 0 }}>
+          <IconButton className="dragHandle" aria-label="drag" size="medium">
+            <Tooltip title="拖曳" placement="top">
+              <DragHandleIcon />
+            </Tooltip>
+          </IconButton>
+        </CardActions>
+
+        <CardContent sx={{ flexGrow: 1, py: 4, textAlign: 'center' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            gutterBottom
+            sx={{
+              color: !item?.stockId ? theme.palette.text.disabled : 'inherit',
+            }}
+          >
+            {item?.stockId ? item?.stockName : item?.stockSeq + 1}
+          </Typography>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary">
+            {/* {item?.price} */}
+            {item?.stockId
+              ? new Intl.NumberFormat('zh-TW', {
+                  style: 'currency',
+                  currency: 'TWD',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                }).format(item?.price)
+              : '_'}
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
 const GridSortable = () => {
-  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  const [draftList, setDraftList] = useState([]);
+  const localStorageKey = 'gridSortableData';
 
-  // Fetching the data from typicode
-  // and setting it to "users" state
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-      .then((res) => res.json())
-      .then((data) =>
-        setUsers((prev) => {
-          return data.map((item) => ({
-            id: item.id,
-            name: item.title,
-          }));
-        }),
-      );
+    const storedData = localStorage.getItem(localStorageKey);
+
+    if (storedData) {
+      setList(JSON.parse(storedData));
+    } else {
+      fetch('/src/assets/data/data.json')
+        .then((res) => res.json())
+        .then((data) => {
+          setList(data.map((item) => ({ ...item })));
+          localStorage.setItem(localStorageKey, JSON.stringify(data));
+        });
+    }
   }, []);
+
+  useEffect(() => {
+    console.log('list updated:', list);
+  }, [list]);
+
+  useEffect(() => {
+    console.log('draftList updated:', draftList);
+    setList(draftList);
+  }, [draftList]);
 
   // Drag and Drop Handler
   const handleDragDropEnds = (oldIndex, newIndex) => {
     console.log('Drag and drop other tasks');
     console.log(oldIndex, newIndex);
+    // list[0].stockList[oldIndex].stockSeq = newIndex;
+    // list[0].stockList[newIndex].stockSeq = oldIndex;
+    // console.log('updatedList:::', list[0]);
+    const updatedList = list[0]?.stockList.map((item, index) => ({
+      ...item,
+      stockSeq: index,
+    }));
+    console.log('updatedList:::', updatedList);
+
+    // setDraftList((prevList) => {
+    //   const newListCopy = [...prevList];
+    //   newListCopy[0].stockList = updatedList;
+    //   return newListCopy;
+    // });
+    setDraftList([{ ...list[0], stockList: updatedList }]);
+  };
+
+  const handleSetList = (newList) => {
+    const updatedList = newList.map((item, index) => ({
+      ...item,
+      stockSeq: index,
+    }));
+
+    setList([{ ...list[0], stockList: updatedList }]);
   };
 
   return (
@@ -74,9 +146,10 @@ const GridSortable = () => {
 
         '& .grid-container': {
           display: 'grid',
-          gap: '16px',
+          gap: '0',
           gridTemplateColumns: 'repeat(3, 1fr)',
           gridTemplateRows: '100%',
+          overflowY: 'hidden',
         },
 
         '& .dropArea': {
@@ -94,25 +167,31 @@ const GridSortable = () => {
         },
       }}
     >
-      {users.length === 0 ? (
+      {list.length === 0 ? (
         <GridAddItem />
       ) : (
         <ReactSortable
-          list={users}
-          setList={(newlist) => setUsers(newlist)}
+          list={list[0].stockList}
+          setList={(newList) => {
+            // setList([{ ...list[0], stockList: newList }]);
+            // console.log('newList:::', newList);
+            handleSetList(newList);
+          }}
           className="grid-container"
           ghostClass="dropArea"
           handle=".dragHandle"
           filter=".ignoreDrag"
           preventOnFilter={true}
-          onEnd={({ oldIndex, newIndex }) =>
-            handleDragDropEnds(oldIndex, newIndex)
-          }
+          // onEnd={({ oldIndex, newIndex }) =>
+          //   handleDragDropEnds(oldIndex, newIndex)
+          // }
         >
-          {users.map((user) => (
-            <GridItem key={user.id} user={user} />
+          {list?.[0]?.stockList.map((item, index) => (
+            <GridItem key={item.stockSeq} item={item} />
           ))}
-          <GridAddItem />
+          {/* {list?.[0].stockList.map((item, index) => (
+            <GridItem key={item.stockId} item={item} />
+          ))} */}
         </ReactSortable>
       )}
     </Box>
